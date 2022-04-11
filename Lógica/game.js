aad42c8b5e711e605fae4b465c97592ae300315d
Lincon Vinicius <99558382+linconvinicius@users.noreply.github.com>
2022-04-11 17:11:37 -0300

    Jogo da Memoria

---
+++ Lógica/game.js
@@ -0,0 +1,110 @@
+let game = {
+
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
+    escudos: ['ajax',
+        'barça',
+        'bayern',
+        'borussia',
+        'city',
+        'juventus',
+        'liverpool',
+        'psg',
+        'real',
+        'inter'],
+
+    cards: null,
+
+    setCard: function (id) {
+
+        let card = this.cards.filter(card => card.id === id)[0];
+        console.log(card);
+        if (card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if (!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        } else {
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        }
+
+    },
+
+    checkMatch: function () {
+        if (!this.firstCard || !this.secondCard) {
+            return false;
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+    },
+
+    clearCards: function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+    unflipCards() {
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
+    checkGameOver() {
+
+        return this.cards.filter(card => !card.flipped).length == 0;
+    },
+
+    createCardsFromEscudos: function () {
+
+        this.cards = [];
+
+        this.escudos.forEach((escudo) => {
+            this.cards.push(this.createPairFromEscudos(escudo));
+        })
+        this.cards = this.cards.flatMap(pair => pair);
+        this.shuffleCards();
+        return this.cards;
+    },
+
+    createPairFromEscudos: function (escudo) {
+
+        return [{
+            id: this.createIdWithEscudos(escudo),
+            icon: escudo,
+            flipped: false,
+        }, {
+            id: this.createIdWithEscudos(escudo),
+            icon: escudo,
+            flipped: false,
+        }]
+
+    },
+
+    createIdWithEscudos: function (escudo) {
+        return escudo + parseInt(Math.random() * 1000);
+    },
+
+    shuffleCards: function (cards) {
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+
+        while (currentIndex !== 0) {
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]]
+        }
+
+    }
+
+
+
+}
